1c96c22b803083dbb7e34f530daba619790b1596
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,60 +1,55 @@
-output "fluid_relay_servers" {
-  description = "All fluid_relay_server resources"
-  value       = azurerm_fluid_relay_server.fluid_relay_servers
-  sensitive   = true
-}
 output "fluid_relay_servers_customer_managed_key" {
-  description = "List of customer_managed_key values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.customer_managed_key]
+  description = "Map of customer_managed_key values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.customer_managed_key }
 }
 output "fluid_relay_servers_frs_tenant_id" {
-  description = "List of frs_tenant_id values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.frs_tenant_id]
+  description = "Map of frs_tenant_id values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.frs_tenant_id }
 }
 output "fluid_relay_servers_identity" {
-  description = "List of identity values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.identity]
+  description = "Map of identity values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.identity }
 }
 output "fluid_relay_servers_location" {
-  description = "List of location values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.location]
+  description = "Map of location values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.location }
 }
 output "fluid_relay_servers_name" {
-  description = "List of name values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.name]
+  description = "Map of name values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.name }
 }
 output "fluid_relay_servers_orderer_endpoints" {
-  description = "List of orderer_endpoints values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.orderer_endpoints]
+  description = "Map of orderer_endpoints values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.orderer_endpoints }
 }
 output "fluid_relay_servers_primary_key" {
-  description = "List of primary_key values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.primary_key]
+  description = "Map of primary_key values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.primary_key }
   sensitive   = true
 }
 output "fluid_relay_servers_resource_group_name" {
-  description = "List of resource_group_name values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.resource_group_name]
+  description = "Map of resource_group_name values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.resource_group_name }
 }
 output "fluid_relay_servers_secondary_key" {
-  description = "List of secondary_key values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.secondary_key]
+  description = "Map of secondary_key values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.secondary_key }
   sensitive   = true
 }
 output "fluid_relay_servers_service_endpoints" {
-  description = "List of service_endpoints values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.service_endpoints]
+  description = "Map of service_endpoints values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.service_endpoints }
 }
 output "fluid_relay_servers_storage_endpoints" {
-  description = "List of storage_endpoints values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.storage_endpoints]
+  description = "Map of storage_endpoints values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.storage_endpoints }
 }
 output "fluid_relay_servers_storage_sku" {
-  description = "List of storage_sku values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.storage_sku]
+  description = "Map of storage_sku values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.storage_sku }
 }
 output "fluid_relay_servers_tags" {
-  description = "List of tags values across all fluid_relay_servers"
-  value       = [for k, v in azurerm_fluid_relay_server.fluid_relay_servers : v.tags]
+  description = "Map of tags values across all fluid_relay_servers, keyed the same as var.fluid_relay_servers"
+  value       = { for k, v in azurerm_fluid_relay_server.fluid_relay_servers : k => v.tags }
 }
 
